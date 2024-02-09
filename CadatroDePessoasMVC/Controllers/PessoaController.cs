3dbd9f063e01f11e52b94481c47aa1c0b90886f6
MARIA GABRIELLY GINO MAIERO <maria.ggmaiero@senacsp.edu.br>
2024-02-08 21:42:04 -0300

    criando projeto

---
+++ CadatroDePessoasMVC/Controllers/PessoaController.cs
@@ -0,0 +1,37 @@
+﻿using CadatroDePessoasMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.PathSegments;
+using System.Data;
+using System.Runtime.ConstrainedExecution;
+
+namespace CadatroDePessoasMVC.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListaPessoas()
+        {
+            //to do: conectar o banco de dados, como nao temos
+            //vamos criar dados fake
+
+            List<Pessoa> pessoas = new List<Pessoa>();
+            pessoas.Add(new Pessoa { CPF = 15246252962, Nome = "maria", datanascimento = DateTime.Now.AddDays(25), Endereco = "aaaaaaaaa", telefone = 940359251, email = "maria.maiero@gmail.com", nomemae = "maria", sexo = "feminino", salario = 10000, cor = "parda" });
+            pessoas.Add(new Pessoa { CPF = 15246252962, Nome = "maria", datanascimento = DateTime.Now.AddDays(25), Endereco = "aaaaaaaaa", telefone = 940359251, email = "maria.maiero@gmail.com", nomemae = "maria", sexo = "feminino", salario = 10000, cor = "parda" });
+            pessoas.Add(new Pessoa { CPF = 15246252962, Nome = "maria", datanascimento = DateTime.Now.AddDays(25), Endereco = "aaaaaaaaa", telefone = 940359251, email = "maria.maiero@gmail.com", nomemae = "maria", sexo = "feminino", salario = 10000, cor = "parda" });
+            
+            return View(pessoas);
+
+        }
+
+        public IActionResult criarpessoas() 
+        { 
+            
+            return View();
+            
+        }
+
+    }
+}
---
+++ CadatroDePessoasMVC/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿namespace CadatroDePessoasMVC.Models
+{
+    public class Pessoa
+    {
+        public long CPF { get; set; }        
+        public string Nome { get; set; }
+        public DateTime datanascimento { get; set; }
+        public string Endereco { get; set; }
+        public long telefone { get; set; }
+        public string email { get; set; }
+        public string nomemae {  get; set; }
+        public string sexo { get; set; }
+        public decimal salario { get; set; }
+        public string cor {  get; set; }
+        
+        
+    }
+}
